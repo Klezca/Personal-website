fa012c2803d8b5d940b2a1f6f29665f9adf14d71
Norman Wong <nich19032@outlook.com>
2020-04-29 22:34:10 +1000

    Initial commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/blog/2020/dogs')
+def dog():
+    return 'This is my dog'
