c80d638bb8bed3a790de09f95d6d2370da6b5d52
Norman Wong <nich19032@outlook.com>
2020-04-30 17:22:52 +1000

    Added a quick setup for storing contact data
    Signed-off-by: Norman Wong <nich19032@outlook.com>

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -11,3 +11,21 @@ def index():
 @app.route('/<string:page_name>')
 def html_page(page_name=None):
     return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+@app.route('/submit-form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong'
