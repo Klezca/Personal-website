43548e38ea8ad1b45f869d92992410e2879f282b
Norman Wong <nich19032@outlook.com>
2020-04-30 15:24:47 +1000

    Added dynamic routing to html page url

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ print(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
-@app.route('/blog/2020/dogs')
-def dog():
-    return 'This is my dog'
+@app.route('/<string:page_name>')
+def html_page(page_name=None):
+    return render_template(page_name)
